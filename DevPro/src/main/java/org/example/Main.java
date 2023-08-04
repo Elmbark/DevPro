e250e9907840903ec6d2e8051b3e96d8ce0b4f92
nidev <elmbarkinidhal@gmail.com>
2023-08-04 10:53:07 +0100

    here im testing the commit

--- DevPro/src/main/java/org/example/Main.java
+++ DevPro/src/main/java/org/example/Main.java
@@ -6,11 +6,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
         System.out.println("this is my first commit test ");
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
+        for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
     }
